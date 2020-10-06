53b84bb9619f3ebcedad7d554621959a8d0283a1
kareem kasem <kareemkasem@outlook.com>
2020-10-06 20:03:10 +0200

    sending email confirmation works

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,20 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 // ...............................................................
+
+const mailTransporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
+
+const ADMIN_EMAIL = "kariimkasem@gmail.com";
 
 exports.getSignIn = (req, res, next) => {
   res.render("auth/signin", {
@@ -35,6 +48,7 @@ exports.postSignUp = async (req, res, next) => {
     });
   }
 
+  // error checking ....................................
   if (!errors.isEmpty()) {
     reloadWithError(errors.array({ onlyFirstError: true })[0].msg);
     return;
@@ -55,6 +69,7 @@ exports.postSignUp = async (req, res, next) => {
     reloadWithError();
     return;
   }
+  // ......................................................
 
   try {
     password = await bcrypt.hash(password, 12);
@@ -66,13 +81,35 @@ exports.postSignUp = async (req, res, next) => {
     });
 
     await user.save();
-    res.render("auth/signin", {
-      path: "/signin",
-      pageTitle: "sign in",
-      errorMessage: null,
-      oldInput: null,
-    });
+
+    // sending a confirmation email
+    try {
+      await mailTransporter.sendMail({
+        from: ADMIN_EMAIL,
+        to: email,
+        subject: "signup confirmation",
+        html: `
+          <h3>hi ${name.split(" ")[0]}!</h3>
+          <h3>you now have an account on metro cinema</h3>
+          <h5>check out our <a href="http://localhost:8000">latest movies</a> and get your ticket with a few clicks!</h5>
+          </div>
+          `,
+      });
+
+      res.render("auth/signin", {
+        path: "/signin",
+        pageTitle: "sign in",
+        errorMessage: null,
+        oldInput: null,
+      });
+    } catch (err) {
+      console.log(err);
+      reloadWithError(
+        "user created successfully but confirmation email couldn't be sent"
+      );
+    }
   } catch (err) {
+    console.log(err);
     reloadWithError();
     return;
   }
